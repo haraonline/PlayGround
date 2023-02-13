0c1a52acc6173e88e62bac82afbb0da5652c92ad
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-02-13 08:19:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayGround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
