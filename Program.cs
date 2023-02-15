fa53e892c38063994db5e1442b95aa807bbfa6d7
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-02-15 14:22:13 +0100

    reflections 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace PlayGround
+﻿using System.Reflection;
+
+namespace PlayGround
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //class of type
+            Type someType;
+
+            //one way of checking the class
+            Person somePerson = new Person();
+            someType = somePerson.GetType();
+
+            //another way
+            Type personType = typeof(Person);
             
         }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Address { get; set; }
+
+
+            public void print()
+            {
+                Console.WriteLine($"Name: {Name} Age: {Age} Address: {Address}");
+
+            }
+
+            public void ChangeAddresses(string newAddress)
+            {
+                Address = newAddress;
+                Console.WriteLine($"Name: {Name} Age: {Age} has a new Address: {Address}");
+            }
+        }        
     }
 }
